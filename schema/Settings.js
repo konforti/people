0af5c1b7b04f58912d3140df2f41950a2c575f16
Raz Konforti <konforti@gmail.com>
2015-05-26 17:35:26 +0300

    settings page

--- schema/Settings.js
+++ schema/Settings.js
@@ -8,4 +8,20 @@ exports = module.exports = function(app, mongoose) {
   settingsSchema.index({ value: 1 });
   settingsSchema.set('autoIndex', (app.get('env') === 'development'));
   app.db.model('Settings', settingsSchema);
+
+  settingsSchema.statics.varGet = function(id, done) {
+    settingsSchema.findById(id).exec(function (err, param) {
+      if (err) {
+        if (typeof done === 'function') {
+          return done(err);
+        }
+        return err;
+      }
+
+      if (typeof done === 'function') {
+        return done(param);
+      }
+      return param;
+    })
+  };
 };
