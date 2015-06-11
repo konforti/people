e20a1d1f95c50e9f0e0522d122c9cef7100df7fc
Raz Konforti <konforti@gmail.com>
2015-06-11 14:19:51 +0300

    -

--- config.example.js
+++ config.example.js
@@ -9,6 +9,3 @@ exports.mongodb = {
 // user 1 (master) ID.
 // Can not be update or delete form the UI. Only straight form the DB.
 exports.uid1 = '';
-exports.cryptoKey = 'k3yb0ardc4t';
-exports.public_key = 'pk_BDBFEA65CB85DC41FF9E928E7886C';
-exports.secret_key = 'sk_A6D8FC87413B789E4E9E86FAC43A2';
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
 'use strict';
 
 require("./app.js");
-
